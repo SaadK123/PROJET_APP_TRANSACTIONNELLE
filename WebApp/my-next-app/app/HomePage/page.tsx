8bcd75176305cf6ca3c1f659399c44bae8409205
CCharbel-eid <2313443@bdeb.qc.ca>
2026-02-16 12:06:34 -0500

    work done for HomePage during free time. making Header look amazing

--- WebApp/my-next-app/app/HomePage/page.tsx
+++ WebApp/my-next-app/app/HomePage/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 import { useRouter } from "next/navigation";
 export default function HomePage() {
+
   const router = useRouter();
   const gotosignup = () => {
     router.push("../page");
   };
+  
   return (
     <div className="homepage-background min-h-screen bg-cover bg-center">
     <div className="container-fluid ">
       <div className="row bg-white ">
-        <div className="col-8 p-3"></div>
-        <div className="col-2 ps-3 pe-3 pt-2 pb-2 m-2">
-          <button className="p-2 text-dark" type="button">
-            Produit
-          </button>
-          <button className="col-4 ps-3 pe-3 pt-2 pb-2 m-2 text-white bg-dark"type="button">Forfait</button>
-          <button className="ps-3 pe-3 pt-2 pb-2 m-2 text-white bg-primary" type="button">Contact</button>
-        </div>
+        <div className="col-1 p-3 "><img src="LogoLinkUp.png"/></div>
+        <div className="col-9 p-3 pe-5 text-end" >
+          <button className=" ps-2 pe-2 text-dark rounded bg-gray-300"type="button">Produit</button>
+          <button className=" ps-2 pe-2 ms-2 me-2 text-dark rounded bg-gray-300"type="button">Forfait</button>
+          <button className=" ps-2 pe-2 ms-2 me-2 text-dark rounded bg-gray-300" type="button">Contact</button>
+         </div>
+        <div className="col-2 text-center p-3">
+       <button className="ps-3 pe-3" type="button">Connection</button>
+        <button className="ps-3 pe-3 rounded bg-green-500 text-white" type="button">Inscription</button></div> 
+         </div>
       </div>
-    </div>
     </div>
   );
 }
