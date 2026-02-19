6ce131914d63a98fa8138779d80fb5f6fe8ee208
CCharbel-eid <2313443@bdeb.qc.ca>
2026-02-19 12:30:54 -0500

    got the Logo working now making sure that it fits well shape wise

--- WebApp/my-next-app/app/HomePage/page.tsx
+++ WebApp/my-next-app/app/HomePage/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
     <div className="homepage-background min-h-screen bg-cover bg-center">
     <div className="container-fluid ">
       <div className="row bg-white ">
-        <div className="col-1"><button><img src="WebApp/my-next-app/app/HomePage/Img/LogoLinkUp.png" alt="Logo"/></button></div>
+        <div className="col-1"><button><img src="/Img/LogoLinkUp.png" alt="Logo"/></button></div>
         <div className="col-9 p-3 pe-5 text-end" >
           <button className=" ps-2 pe-2 text-dark rounded bg-gray-300"type="button">Produit</button>
           <button className=" ps-2 pe-2 ms-2 me-2 text-dark rounded bg-gray-300"type="button">Forfait</button>
