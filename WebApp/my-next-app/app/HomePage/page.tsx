8eb98110f7ac2457b40adca63d0cd9af3d1dadd0
CCharbel-eid <2313443@bdeb.qc.ca>
2026-02-19 11:07:08 -0500

    work on HomePage Aligning header with page and addings logo

--- WebApp/my-next-app/app/HomePage/page.tsx
+++ WebApp/my-next-app/app/HomePage/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
     <div className="homepage-background min-h-screen bg-cover bg-center">
     <div className="container-fluid ">
       <div className="row bg-white ">
-        <div className="col-1 p-3 "><img src="LogoLinkUp.png"/></div>
+        <div className="col-1"><button><img src="WebApp/my-next-app/app/HomePage/Img/LogoLinkUp.png" alt="Logo"/></button></div>
         <div className="col-9 p-3 pe-5 text-end" >
           <button className=" ps-2 pe-2 text-dark rounded bg-gray-300"type="button">Produit</button>
           <button className=" ps-2 pe-2 ms-2 me-2 text-dark rounded bg-gray-300"type="button">Forfait</button>
@@ -19,7 +19,7 @@ export default function HomePage() {
          </div>
         <div className="col-2 text-center p-3">
        <button className="ps-3 pe-3" type="button">Connection</button>
-        <button className="ps-3 pe-3 rounded bg-green-500 text-white" type="button">Inscription</button></div> 
+        <button className="ps-2 pe-2 rounded bg-green-500 text-white" type="button">Inscription</button></div> 
          </div>
       </div>
     </div>
