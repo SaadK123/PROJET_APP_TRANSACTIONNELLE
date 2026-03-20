73ed36b2d6614dac1811a8dad934d1e6f28ef3a0
Marat Sargsyan <marat5059@gmail.com>
2026-03-20 10:53:24 -0400

    ajout de entite conversation pour la messagerie

---
+++ LinkUp/src/main/java/projetweb/linkup/entities/Conversation.java
@@ -0,0 +1,21 @@
+package projetweb.linkup.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "converation")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Conversation {
+}
